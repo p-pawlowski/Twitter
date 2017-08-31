41cf39aab9d47210c5e6e02b3d7ec0b6fd40f3f0
p-pawlowski <piotr.m.pawlowski@gmail.com>
2017-08-31 15:37:41 +0200

    connection with MySql DB established

---
+++ twitter/src/main/java/pawlowski/piotr/twitter/entity/Tweet.java
@@ -0,0 +1,35 @@
+package pawlowski.piotr.twitter.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tweets")
+public class Tweet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String text;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
