9dd42a47503eeb5ff0bb2c8526aebc19c6cc8f1f
p-pawlowski <piotr.m.pawlowski@gmail.com>
2017-09-19 20:52:32 +0200

    update

--- twitter/src/main/java/pawlowski/piotr/twitter/controller/DefaultController.java
+++ twitter/src/main/java/pawlowski/piotr/twitter/controller/DefaultController.java
@@ -3,21 +3,27 @@ package pawlowski.piotr.twitter.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pawlowski.piotr.twitter.entity.Tweet;
+import pawlowski.piotr.twitter.entity.User;
 import pawlowski.piotr.twitter.repository.TweetRepository;
+import pawlowski.piotr.twitter.service.UserService;
 
 @Controller
 public class DefaultController {
 	
 	private TweetRepository tweetRepository;
+	private UserService userService;
 	
 	@Autowired
-	public DefaultController(TweetRepository tweetRepository) {
+	public DefaultController(TweetRepository tweetRepository, UserService userService) {
 		this.tweetRepository = tweetRepository;
+		this.userService = userService;
 	}
 	
 	@RequestMapping(value="/access-denied")
@@ -27,7 +33,12 @@ public class DefaultController {
 	
 	@RequestMapping(value="/")
 	public String mainPage(Model model){
-
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+//		if (user == null){
+//			user = new User();
+//		}
+		model.addAttribute("user", user);
 		List<Tweet> tweetList = (List<Tweet>) tweetRepository.findFirst10ByOrderByIdAsc();
 		model.addAttribute("tweetList", tweetList);
 		return "main-page";
